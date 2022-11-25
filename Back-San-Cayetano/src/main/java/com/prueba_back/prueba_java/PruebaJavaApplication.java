9d2411e4c17eea92fabb431a948be42d326439f7
Juanfe2008 <wardkjuan44@gmail.com>
2022-11-25 17:35:12 -0500

    config eureka

--- Back-San-Cayetano/src/main/java/com/prueba_back/prueba_java/PruebaJavaApplication.java
+++ Back-San-Cayetano/src/main/java/com/prueba_back/prueba_java/PruebaJavaApplication.java
@@ -2,10 +2,12 @@ package com.prueba_back.prueba_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class PruebaJavaApplication {
 
     public static void main(String[] args) {
--- Sistemas_Entidades/src/main/java/com/corhuila/sistemas/SeguridadApplication.java
+++ Sistemas_Entidades/src/main/java/com/corhuila/sistemas/SeguridadApplication.java
@@ -2,11 +2,13 @@ package com.corhuila.sistemas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class SeguridadApplication {
 
 	public static void main(String[] args) {
