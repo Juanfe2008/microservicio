dbad2dab7843438485d6f353c94e647d299c37b8
Juanfe2008 <wardkjuan44@gmail.com>
2022-11-28 15:08:16 -0500

    multiple instances

--- Sistemas_Entidades/src/main/java/com/corhuila/sistemas/feignclients/ProductoFeingClient.java
+++ Sistemas_Entidades/src/main/java/com/corhuila/sistemas/feignclients/ProductoFeingClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "productos", url = "http://localhost:8080")
+@FeignClient(name = "prueba-service")
 @RequestMapping("/api/products")
 public interface ProductoFeingClient {
 
