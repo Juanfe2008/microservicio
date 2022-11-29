c8c1fbb9c5468107147bbca8a4fca5ec76e10eaa
Juanfe2008 <wardkjuan44@gmail.com>
2022-11-29 11:50:52 -0500

    ajuste con rutas

--- Sistemas_Entidades/src/main/java/com/corhuila/sistemas/controller/MarcasControler.java
+++ Sistemas_Entidades/src/main/java/com/corhuila/sistemas/controller/MarcasControler.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("api/marca ")
+@RequestMapping("api/marca")
 public class MarcasControler {
 
     @Autowired
